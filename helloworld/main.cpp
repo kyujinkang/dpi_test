7caab1bc8a8f85f1db70a0f342f976de206b7d27
JunwonKong <jjunwonny@gmail.com>
2016-01-27 16:36:05 +0900

    implement_conflict_function

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -14,6 +14,10 @@ int multi(int value1, int value2) {
 	return value1 * value2;
 }
 
+int numberMulti(int a, int b, int c) {
+	return a*b*c;
+}
+
 int main() {
 	cout << "Hello world!\n" << endl;
 	return 0;
